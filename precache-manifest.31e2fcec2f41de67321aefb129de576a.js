1136ef3b3c2234b5b38f0357870f999bdca63d07
icarpio <ivancg24@gmail.com>
2019-02-24 10:52:10 +0100

    Cotizador Seguros from Rapsberry

---
+++ precache-manifest.31e2fcec2f41de67321aefb129de576a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53b7e265787e4709478a",
+    "url": "/static/css/main.88b65273.chunk.css"
+  },
+  {
+    "revision": "53b7e265787e4709478a",
+    "url": "/static/js/main.53b7e265.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c5798586f9d10d3d527c",
+    "url": "/static/js/2.c5798586.chunk.js"
+  },
+  {
+    "revision": "cb67585f54a4d5322909555eec162701",
+    "url": "/index.html"
+  }
+];
